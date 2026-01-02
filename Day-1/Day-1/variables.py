6ebe0b009051256384aca45aae22cae879724b77
sakshidobhal15 <dobhalsakshi075@gmail.com>
2026-01-02 18:33:11 +0530

    Day 1:Variables program

---
+++ Day-1/Day-1/variables.py
@@ -0,0 +1,13 @@
+# Day 1 - Variables
+
+# Storing values in variables
+name = "Sakshi"
+age = 20
+is_student = True
+height = 5.4
+
+# Printing variables
+print("Name:", name)
+print("Age:", age)
+print("Student:", is_student)
+print("Height:", height)
