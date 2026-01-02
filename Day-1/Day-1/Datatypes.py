5af5170024f459ffae9b66aeacaa0ebb99d0b38b
sakshidobhal15 <dobhalsakshi075@gmail.com>
2026-01-02 18:38:05 +0530

    Day 1:Data types program

---
+++ Day-1/Day-1/Datatypes.py
@@ -0,0 +1,22 @@
+# Day 1 - Data Types
+# Examples of different data types
+
+# Integer
+x = 10
+print("Integer:", x, "Type:", type(x))
+
+# Float
+y = 5.5
+print("Float:", y, "Type:", type(y))
+
+# String
+name = "Sakshi"
+print("String:", name, "Type:", type(name))
+
+# Boolean
+is_student = True
+print("Boolean:", is_student, "Type:", type(is_student))
+
+# List
+fruits = ["apple", "banana", "cherry"]
+print("List:", fruits, "Type:", type(fruits))
