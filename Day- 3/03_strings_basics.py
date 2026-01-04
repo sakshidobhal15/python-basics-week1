84d7308c5b53f569a73cdec12c04e6ddccadaf7e
Sakshi <dobhalsakshi075@gmail.com>
2026-01-04 22:20:09 +0530

    Day 3: Strings basics

---
+++ Day- 3/03_strings_basics.py
@@ -0,0 +1,14 @@
+# strings.py
+# Basic string concepts in Python
+
+name = "Python Programming"
+
+print("String:", name)
+print("First character:", name[0])
+print("Last character:", name[-1])
+
+print("Slicing (0 to 6):", name[0:6])
+
+print("Traversing string:")
+for ch in name:
+    print(ch)
